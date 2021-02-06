a22e7dd2d0b33204664ea2b68278a94f9c0197c5
17105873 <17105873@stu.mmu.ac.uk>
2021-02-06 19:54:24 +0000

    Added navigation pages and login screen

--- coffida/App.js
+++ coffida/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
+import Login from './components/Login';
+
 class HelloWorldApp extends Component{
   render(){
+
+  const name = "Luke";
+
     return (
         <View>
-          <Text>Hello World</Text>
+          <Login />
         </View>
     );
   }
